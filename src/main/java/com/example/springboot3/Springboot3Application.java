3c6cda64ebcb7091a9a819703113ba983e9805d3
Wisney <wisneymaciel2@hotmail.com>
2024-04-21 12:35:35 -0300

    inserting HATEOAS to products

--- src/main/java/com/example/springboot3/Springboot3Application.java
+++ src/main/java/com/example/springboot3/Springboot3Application.java
@@ -1,8 +1,11 @@
 package com.example.springboot3;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration;
 
+@EnableAutoConfiguration(exclude = HypermediaAutoConfiguration.class)
 @SpringBootApplication
 public class Springboot3Application {
 
